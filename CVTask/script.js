974e0ec255c85d19401136715c98ed740f36ee02
elistribley <eli_stribley@hotmail.com>
2022-05-18 10:47:23 +0100

    including JS with HTML&CSS ES

---
+++ CVTask/script.js
@@ -0,0 +1,28 @@
+console.log("Eli");
+console.log("Stribley");
+console.log("Tiverton");
+console.log("Aquarius");
+
+let myFavCar = "McLaren";
+let myFavModel = "620R";
+
+console.log(`my favourite car is ${myFavCar} and the model is ${myFavModel}`,
+);
+console.log("%c Yea Yea","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
+
+let a = "";
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+console.log(typeof(a), typeof(b), typeof(c), typeof(d), typeof(e));
+
+
+
+
+
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = totalMoney - moneyPaidSoFar;
+console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`);
