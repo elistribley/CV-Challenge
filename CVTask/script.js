04d7a3dd107c72ab8565f62791f15dc87399e32f
elistribley <eli_stribley@hotmail.com>
2022-05-18 12:25:31 +0100

    Including varibles using booleans, strings and numbers ES

--- CVTask/script.js
+++ CVTask/script.js
@@ -26,3 +26,13 @@ let totalMoney = 4000;
 let moneyPaidSoFar = 2348;
 let totalLeftToPay = totalMoney - moneyPaidSoFar;
 console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`);
+
+let nono = false;
+let str1 = "5 + 3 ="
+let equ1 = 4*7/2+7-1;
+let str2 = "This is an uppercase string";
+str2.toUpperCase;
+console.log(str2);
+console.log(str1 = 5+3);
+console.log(equ1);
+console.log(str1 == nono);
