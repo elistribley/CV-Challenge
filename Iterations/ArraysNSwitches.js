d699e87a95271d2ccd8845e9ef5d9742833f843e
elistribley <eli_stribley@hotmail.com>
2022-05-19 11:57:07 +0100

    Added and extended arrays ES

--- Iterations/ArraysNSwitches.js
+++ Iterations/ArraysNSwitches.js
@@ -1,29 +1,31 @@
 'use strict';
 
 let darthVader = {
+    famousQuote: "'I am your father'",
     allegiance: "Empire",
     weapon: "lightsaber",
     sith: true
 };
 
-
+console.log(`Darth Vader once told Luke Skywalker... ${darthVader.famousQuote}`);
 console.log(`Darth Vader's allegiance is to the ${darthVader.allegiance}`);
 console.log(`Darth Vader's weapon of choice is a ${darthVader.weapon}`);
 console.log(`Darth Vader is a Sith? ${darthVader.sith}`);
 console.log(`Darth Vader is a Jedi? ${darthVader.sith ? "false" : "true"}`)
-
+console.log('');
 let obiWanKenobi = {
+    famousQuote: "'Hello there'",
     allegiance: "Jedi Council and Republic",
     weapon: "Lightsaber",
     sith: false
 };
 
-
+console.log(`Obi Wan Kenobi loves saying ${obiWanKenobi.famousQuote}`);
 console.log(`Obi Wan Kenobi's allegiance is to the ${obiWanKenobi.allegiance}`);
 console.log(`Obi Wan Kenobi's weapon of choice is a ${obiWanKenobi.weapon}`);
 console.log(`Obi Wan Kenobi is a Sith? ${obiWanKenobi.sith}`);
 console.log(`Obi Wan Kenobi is a Jedi? ${obiWanKenobi.sith ? "false" : "true"}`)
-
+console.log('');
 
 let myArray = ["hello",'everyone'];
  console.log(myArray.length);
